aa944ccd9ef5405a709f6ce3ce4ee53d49cee5a9
Henrik Österman <henos134@li4-12.ida.liu.se>
2015-05-11 10:59:18 +0200

    added simple transition function

--- engine/globals.rkt
+++ engine/globals.rkt
@@ -3,8 +3,5 @@
 
 (define *current-act* #f)
 
-#| (define (change-act to)
-  (if (transition? to) 
-      ;transition? måste skapas, uppgift att kolla om en övergång finns mellan *current-act* och den spelaren vill till
-      (set! *current-act* to)
-      (error "you can't go there"))) |#
+(define (transition destination) ;enkel implementering för tillfället
+  (set! *current-act* destination)) ;så vi kan testa koden
