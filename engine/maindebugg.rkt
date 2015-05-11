9b71c45d3acfd35ec0c5c34cf3289248b58e8687
Henrik Österman <henos134@li4-12.ida.liu.se>
2015-05-11 11:00:47 +0200

    testfile

---
+++ engine/maindebugg.rkt
@@ -0,0 +1,24 @@
+#lang racket
+(require "actclass.rkt")
+(require "granpaclass.rkt")
+(require "globals.rkt")
+(require "leveldatastructure.rkt")
+(require compatibility/mlist)
+
+
+
+;test
+(define test-level (make-new-level 6 5))
+(define test-level2 (make-new-level 6 5))
+(level-set-object-at-pos! test-level (list 6 5) 'hej)
+(define mamma
+  (new granpa%
+       [name "mamma"])) 
+(define test-act
+  (new act% 
+       [levels (mlist test-level test-level2)]
+       [name "test-act"]))
+(transition test-act) ;ändrar *current-act* till test-act
+(send *current-act* move-obj mamma #f '(1 1 1)) ;ändrar mammas position till (1 1 1) från #f
+
+(send mamma set-pos! '(2 2 2) *current-act*) ;ska inte resultera i något då acten har en annan position
