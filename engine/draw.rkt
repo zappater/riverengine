eef6c8184f6ec72d533fc8a293a0838dfab39adb
Erik Bäcklund Ekvall <eriek286@li3-10.ida.liu.se>
2015-05-20 14:28:33 +0200

    debugged draw

--- engine/draw.rkt
+++ engine/draw.rkt
@@ -1,13 +1,49 @@
 #lang racket
 (define Game_name "example")
-
+(require compatibility/mlist)
 (require racket/gui/base)
+(require "actclass.rkt")
+(require "granpaclass.rkt")
+(require "leveldatastructure.rkt")
+(require "supportfunctions.rkt")
+(require "globals.rkt")
 
-
+;;DEBUG
 (define viewsize (cons 10 15))
 (define state 0)
+(define game #t)
+(define lvl-1 (make-new-level 40 40))
+(define lvl-2 (make-new-level 40 40))
+(define bluetexture (new granpa%
+                         [texture (read-bitmap
+                                   "testtexture1.png")
+                                  ]))
+(define (fill-level lvl obj pos)
+  (if (< (car pos) 41)
+      (begin
+        (fill-level-row lvl obj pos)
+      (fill-level lvl obj (cons (+ (car pos) 1) (cdr pos))))
+      (void)))
+  
+(define (fill-level-row level obj pos)
+      (if (< (cdr pos) 41)
+      (begin
+        (display pos)
+        (fill-level-row level obj (cons (car pos) (+ 1 (cdr pos))))
+        (level-set-object-at-pos! level (list (car pos) (cdr pos)) obj))
+      (void)))
+(fill-level lvl-1 bluetexture (cons 1 1))
+(define test-act
+  (new act% 
+       [levels (mlist lvl-1 lvl-2)]
+       [name "test-act"]))
+(transition test-act)
 
-
+(define camera-obj (new granpa%
+                        [texture (read-bitmap "ydot.png")]))
+(send camera-obj move-me (list 2 1 1))
+(display (send camera-obj my-pos))
+;;END DEBUG
 ;; tick!: -> void
 (define (tick!)
   (set! state (add1 state))
@@ -19,42 +55,59 @@
   (send dc set-background (make-object color% 0 0 0))
   (send dc clear)
   
-  (define (paint-act! act camera-pos)
-    (draw-enteties (send act get-levels) (send camera-obj my-pos))
-    (define (draw-entetises stucture)
-      (define (draw-level level camera-pos drawpos ypos)
-        (define (draw-row row cameraxpos drawpos xpos)
+  (define (paint-act! act)
+    (draw-enteties (send act get-levels) (cons (second (send camera-obj my-pos)) (third (send camera-obj my-pos))))
+    
+    )
+        
+    (define (draw-level level camera-pos drawpos ypos)
+        (if (null? level)
+            (void)
+            (if (<= (abs (- (car camera-pos) ypos)) (round (/ (car viewsize) 2)))
+                (begin
+                  ;;(display camera-pos)
+                  (display ypos)
+                  (display "\n")
+                  (draw-row (mcar level) (cdr camera-pos) drawpos 1)
+                  (draw-level (mcdr level) camera-pos (cons (+ (car drawpos) 1) (cdr drawpos)) (+ ypos 1)))
+                (draw-level (mcdr level) camera-pos drawpos (+ ypos  1)))))
+
+(define (draw-row row cameraxpos drawpos xpos)
           (if (null? row)
               (void)
               (cond
-                ((xpos < (round (/ (car veiwsize) 2)))  (draw-row (cdr row) cameraxpos drawpos (+ xpos  1)))
-                ((xpos > (round (/ (car veiwsize) 2))) (void))
+                ((< xpos (- cameraxpos (round (/ (cdr viewsize) 2))))  (draw-row (mcdr row) cameraxpos drawpos (+ xpos  1)))
+                ((> xpos  (+ cameraxpos (round (/ (cdr viewsize) 2)))) (void))
                 (else
                  (begin
-                   ((let obj (car (car row)))
+                   ;;(display xpos)
+                   (let ((obj (mcar (mcar row))))
                     (if (null? obj)
                         (void)
-                        (send dc drawbitmap (send obj get-currunet-texture) (* 50 (cdr drawpos)) (* 50 (car drawpos))))
-                    (draw-row (cdr row) cameraxpos (cons (car drawpos) (+ 1 (cdr drawpos))) (+ xpos  1))))))))
-        
-        
-        (if (null? level)
-            (void)
-            (if (<= (abs (- (car camera-pos) ypos)) (round (/ (car veiwsize) 2)))
-                (begin
-                  (draw-row (car level) (cdr camera-pos) drawpos 1)
-                  (draw-level (cdr level) camera-pos (cons (+ (car drawpos) 1) (cdr drawpos)) (+ ypos 1)))
-                (draw-level (cdr level) camera-pos drawpos (+ ypos  1))
-                
-                (if (null? stucture)
+                        (if (send obj get-current-texture)
+                            (begin
+                            (send dc draw-bitmap (send obj get-current-texture) (* 50 (cdr drawpos)) (* 50 (car drawpos)))
+                            ;;(display drawpos)
+                            )
+                            (void))))
+                    (draw-row (mcdr row) cameraxpos (cons (car drawpos) (+ 1 (cdr drawpos))) (+ xpos  1)))))))
+(define (draw-enteties structure camera-pos)
+      (if (null? structure)
                     (void)
                     (begin
-                      (draw-level (car structure) camera-pos (cons 0 0) 1)
-                      (draw-enteties (cdr structure) camera-pos))))
-            
-  ;;kommit hit          ))))
+                      (draw-level (mcar structure) camera-pos (cons 0 0) 1)
+                      (draw-enteties (mcdr structure) camera-pos))))    
+        
+                
+                
+   (define (paint-ui-act! ui)
+     (void))
+  ;;kommit hit     
   (if game
+      (begin
       (paint-act! *current-act*)
+      (paint-ui-act! *curent-ui*))
+      void))
 
 
 
@@ -82,3 +135,12 @@
 (define MAIN-TIMER (new timer% 
                         [notify-callback tick!]
                         [interval 1000]))
+
+      
+     
+        
+;;Palla länka vid felsökning !!tas bort efter länkning!!
+
+
+
+
