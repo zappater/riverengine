8d0263e6e4a378242bb1b0924a58d42c6b3700f4
Erik Bäcklund Ekvall <eriek286@li1-7.ida.liu.se>
2015-05-22 17:07:38 +0200

    basic structure for drawing a world and UI

---
+++ engine/UIdefines.rkt
@@ -0,0 +1,76 @@
+#lang racket
+(provide (all-defined-out))
+(define (Insert ele lst pos)
+  (if (<= pos 1)
+      (cons ele lst)
+      (cons (car lst) (Insert ele (cdr lst) (- pos 1)))))
+(define (Find-and-remove ele lst)
+  (if (null? lst)
+      '()
+      (if (eq? ele (car lst))
+          (cdr lst)
+          (cons (car lst) (Find-and-remove ele (cdr lst))))))
+(define UI%
+  (class object%
+    (super-new)
+    (init-field
+     [uipanels '()]
+     )
+    (define/public (add-uipanel panel [pos 1])
+      (set! uipanels (Insert panel uipanels pos)))
+    (define/public (remove-panel panel)
+      (set! uipanels (Find-and-remove panel uipanels)))
+    (define/public (get-uipanels)
+      uipanels)))
+
+(define UIpanel%
+  (class object%
+    (super-new)
+    (init-field
+     [anchor 'topleft] ;;anchor are topleft topcneter topright centerleft centercenter centerright bottomleft bottomcenter bottomright 
+     [size (cons 0 0)]
+     [offset (cons 0 0)]
+     [texture #f]
+     [text #f]
+     [button-effect (void)]
+     )
+    (define/public (group?)
+    #f)
+    (define/public (get-anchor)
+      anchor)
+    (define/public (get-offset)
+      offset)
+    (define/public (get-size)
+      size)
+    (define/public (get-texture)
+      texture)
+    (define/public (get-text)
+      text)
+    (define/public (set-text! string)
+      (set! text string))))
+
+(define UIpanelgroup%
+  (class object%
+    (super-new)
+    (init-field
+     [uipanels '()]
+     [anchor 'topleft]
+     [offset (cons 0 0)]
+     [size (cons 0 0)]
+     )
+    (define/public (group?)
+      #t)
+    (define/public (get-offset)
+      offset)
+    (define/public (get-anchor)
+      anchor)
+    (define/public (get-size)
+      size)
+    (define/public (add-uipanel panel [pos 1])
+      (set! uipanels (Insert panel uipanels pos)))
+    (define/public (remove-panel panel)
+      (set! uipanels (Find-and-remove panel uipanels)))
+    (define/public (get-panel-lst)
+      uipanels)))
+
+    
--- engine/draw.rkt
+++ engine/draw.rkt
@@ -7,28 +7,29 @@
 (require "leveldatastructure.rkt")
 (require "supportfunctions.rkt")
 (require "globals.rkt")
-
+(require "UIdefines.rkt")
 ;;DEBUG
-(define viewsize (cons 10 15))
+(define *scale* 1)
+(define viewsize (cons 5 5))
 (define state 0)
 (define game #t)
 (define lvl-1 (make-new-level 40 40))
 (define lvl-2 (make-new-level 40 40))
 (define bluetexture (new granpa%
                          [texture (read-bitmap
-                                   "testtexture1.png")
+                                   "Natural grass_001.png")
                                   ]))
 (define (fill-level lvl obj pos)
   (if (< (car pos) 41)
       (begin
         (fill-level-row lvl obj pos)
-      (fill-level lvl obj (cons (+ (car pos) 1) (cdr pos))))
+        (fill-level lvl obj (cons (+ (car pos) 1) (cdr pos))))
       (void)))
-  
+
 (define (fill-level-row level obj pos)
-      (if (< (cdr pos) 41)
+  (if (< (cdr pos) 41)
       (begin
-        (display pos)
+        ;;(display pos)
         (fill-level-row level obj (cons (car pos) (+ 1 (cdr pos))))
         (level-set-object-at-pos! level (list (car pos) (cdr pos)) obj))
       (void)))
@@ -41,8 +42,11 @@
 
 (define camera-obj (new granpa%
                         [texture (read-bitmap "ydot.png")]))
-(send camera-obj move-me (list 2 1 1))
-(display (send camera-obj my-pos))
+(send camera-obj move-me (list 2 30 6))
+;;(display (send camera-obj my-pos))
+(define test-ui
+  (new UI%))
+(change-ui test-ui)       
 ;;END DEBUG
 ;; tick!: -> void
 (define (tick!)
@@ -52,6 +56,7 @@
 
 
 (define (paint! a-canvas dc)
+  (send dc scale *scale* *scale*)
   (send dc set-background (make-object color% 0 0 0))
   (send dc clear)
   
@@ -59,55 +64,114 @@
     (draw-enteties (send act get-levels) (cons (second (send camera-obj my-pos)) (third (send camera-obj my-pos))))
     
     )
-        
-    (define (draw-level level camera-pos drawpos ypos)
-        (if (null? level)
-            (void)
-            (if (<= (abs (- (car camera-pos) ypos)) (round (/ (car viewsize) 2)))
-                (begin
-                  ;;(display camera-pos)
-                  (display ypos)
-                  (display "\n")
-                  (draw-row (mcar level) (cdr camera-pos) drawpos 1)
-                  (draw-level (mcdr level) camera-pos (cons (+ (car drawpos) 1) (cdr drawpos)) (+ ypos 1)))
-                (draw-level (mcdr level) camera-pos drawpos (+ ypos  1)))))
-
-(define (draw-row row cameraxpos drawpos xpos)
-          (if (null? row)
-              (void)
-              (cond
-                ((< xpos (- cameraxpos (round (/ (cdr viewsize) 2))))  (draw-row (mcdr row) cameraxpos drawpos (+ xpos  1)))
-                ((> xpos  (+ cameraxpos (round (/ (cdr viewsize) 2)))) (void))
-                (else
-                 (begin
-                   ;;(display xpos)
-                   (let ((obj (mcar (mcar row))))
-                    (if (null? obj)
-                        (void)
-                        (if (send obj get-current-texture)
-                            (begin
-                            (send dc draw-bitmap (send obj get-current-texture) (* 50 (cdr drawpos)) (* 50 (car drawpos)))
-                            ;;(display drawpos)
-                            )
-                            (void))))
-                    (draw-row (mcdr row) cameraxpos (cons (car drawpos) (+ 1 (cdr drawpos))) (+ xpos  1)))))))
-(define (draw-enteties structure camera-pos)
-      (if (null? structure)
-                    (void)
-                    (begin
-                      (draw-level (mcar structure) camera-pos (cons 0 0) 1)
-                      (draw-enteties (mcdr structure) camera-pos))))    
-        
-                
-                
-   (define (paint-ui-act! ui)
-     (void))
+  
+  (define (draw-level level camera-pos drawpos ypos)
+    (if (null? level)
+        (void)
+        (if (<= (abs (- (car camera-pos) ypos)) (round (/ (car viewsize) 2)))
+            (begin
+              ;;(display camera-pos)
+              ;;(display ypos)
+              ;;(display "\n")
+              (draw-row (mcar level) (cdr camera-pos) drawpos 1)
+              (draw-level (mcdr level) camera-pos (cons (+ (car drawpos) 1) (cdr drawpos)) (+ ypos 1)))
+            (draw-level (mcdr level) camera-pos drawpos (+ ypos  1)))))
+  
+  (define (draw-row row cameraxpos drawpos xpos)
+    (if (null? row)
+        (void)
+        (cond
+          ((< xpos (- cameraxpos (round (/ (cdr viewsize) 2))))  (draw-row (mcdr row) cameraxpos drawpos (+ xpos  1)))
+          ((> xpos  (+ cameraxpos (round (/ (cdr viewsize) 2)))) (void))
+          (else
+           (begin
+             ;;(display xpos)
+             (let ((obj (mcar (mcar row))))
+               (if (null? obj)
+                   (void)
+                   (if (send obj get-current-texture)
+                       (begin
+                         (send dc draw-bitmap (send obj get-current-texture) (* 50 (cdr drawpos)) (* 50 (car drawpos)))
+                         ;;(display drawpos)
+                         )
+                       (void))))
+             (draw-row (mcdr row) cameraxpos (cons (car drawpos) (+ 1 (cdr drawpos))) (+ xpos  1)))))))
+  (define (draw-enteties structure camera-pos)
+    (if (null? structure)
+        (void)
+        (begin
+          (draw-level (mcar structure) camera-pos (cons 0 0) 1)
+          (draw-enteties (mcdr structure) camera-pos))))    
+  
+  
+  
+  (define paint-ui-act!
+    (lambda (ui windowsize)      
+      (define (Draw-panel-help obj size) ;;ritar ut panel eller grupp med ett ankar som 0,0
+        (if (send obj group?)
+            (paint-ui-act! obj size)
+            (let ((texture (send obj get-texture))
+                  (text (send obj get-text)))
+              (begin
+                (if texture
+                    (send dc draw-bitmap texture 0 0)
+                    (void))
+                (if text
+                    (send dc draw-text text 0 0) 
+                    (void))))))
+      (define Draw-panel
+        (lambda (obj)
+          ;;Ränka ut ankar positioner
+          (let ((size (send obj get-size))
+                (offset (send obj get-offset))
+                (anchor (send obj get-anchor)))
+            (cond
+              ((eq? anchor 'topleft) (Draw-panel-help obj size))
+              ((eq? anchor 'topcenter)
+               (send dc translate (+ (- (/ 2 (car windowsize)) (round (/ 2 (car size)))) (car offset)) (cdr offset))
+               (Draw-panel-help obj size)
+               (send dc translate (- (+ (- (/ 2 (car windowsize)) (round (/ 2 (car size)))) (car offset))) (- (cdr offset))))
+              ((eq? anchor 'topright) 
+               (send dc translate (+ (- (car windowsize) (car size)) (car offset)) (cdr offset))
+               (Draw-panel-help obj size)
+               (send dc translate (- (+ (- (car windowsize) (car size)) (car offset))) (- (cdr offset))))
+              ((eq? anchor 'centerleft)
+               (send dc translate (car offset) (+ (- (/ 2 (cdr windowsize)) (round (/ 2 (cdr size)))) (cdr offset)))
+               (Draw-panel-help obj size)
+               (send dc translate (- (car offset)) (- (+ (- (/ 2 (cdr windowsize)) (round (/ 2 (cdr size)))) (cdr offset)))))
+              ((eq? anchor 'centercenter)
+               (send dc translate (+ (- (/ 2 (car windowsize)) (round (/ 2 (car size)))) (car offset))  (+ (- (/ 2 (cdr windowsize)) (round (/ 2 (cdr size)))) (cdr offset)))
+               (Draw-panel-help obj size)
+               (send dc translate (- (+ (- (/ 2 (car windowsize)) (round (/ 2 (car size)))) (car offset)))  (- (+ (- (/ 2 (cdr windowsize)) (round (/ 2 (cdr size)))) (cdr offset)))))
+              ((eq? anchor 'centerright)
+               (send dc translate (+ (- (car windowsize) (car size)) (car offset)) (+ (- (/ 2 (cdr windowsize)) (round (/ 2 (cdr size)))) (cdr offset)))
+               (Draw-panel-help obj size)
+               (send dc translate (- (+ (- (car windowsize) (car size)) (car offset))) (- (+ (- (/ 2 (cdr windowsize)) (round (/ 2 (cdr size)))) (cdr offset)))))
+              ((eq? anchor 'bottomleft)
+               (send dc translate (car offset) (+ (- (cdr windowsize) (cdr size)) (cdr offset)))
+               (Draw-panel-help obj size)
+               (send dc translate (- (car offset)) (- (+ (- (cdr windowsize) (cdr size)) (cdr offset)))))
+              ((eq? anchor 'bottomcenter) 
+               (send dc translate (+ (- (/ 2 (car windowsize)) (round (/ 2 (car size)))) (car offset)) (+ (- (cdr windowsize) (cdr size)) (cdr offset)))
+               (Draw-panel-help obj size)
+               (send dc translate (- (+ (- (/ 2 (car windowsize)) (round (/ 2 (car size)))) (car offset))) (- (+ (- (cdr windowsize) (cdr size)) (cdr offset)))))
+              ((eq? anchor 'bottomright)
+               (send dc translate (+ (- (car windowsize) (car size)) (car offset)) (+ (- (cdr windowsize) (cdr size)) (cdr offset)))
+               (Draw-panel-help obj size)
+               (send dc translate (+ (- (car windowsize) (car size)) (car offset)) (+ (- (cdr windowsize) (cdr size)) (cdr offset))))
+              (else
+               (error obj "has an invaild anchor"))))))
+          ;;Slut på ankar postionerna          
+      (for-each Draw-panel (send ui get-uipanels))))
+  
   ;;kommit hit     
   (if game
       (begin
-      (paint-act! *current-act*)
-      (paint-ui-act! *curent-ui*))
-      void))
+        (paint-act! *current-act*)
+        (paint-ui-act! *current-ui* 
+                       (cons (* 50 *scale* (cdr viewsize)) (* 50 *scale* (car viewsize))))) ;viewsize bestämmer antal tiles rutan är
+      void)
+  (send dc scale (/ 1 *scale*) (/ 1 *scale*)))
 
 
 
@@ -115,8 +179,8 @@
                           (super-new)
                           (define/augment (on-close)
                             (custodian-shutdown-all (current-custodian))))
-                        [width (* 50 (cdr viewsize))]
-                        [height (* 50 (car viewsize))]
+                        [width (* 50 *scale* (cdr viewsize))]
+                        [height (* 50 *scale* (car viewsize))]
                         [label Game_name]))
 
 
@@ -136,9 +200,9 @@
                         [notify-callback tick!]
                         [interval 1000]))
 
-      
-     
-        
+
+
+
 ;;Palla länka vid felsökning !!tas bort efter länkning!!
 
 
