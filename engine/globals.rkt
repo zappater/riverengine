544203723b390fde323235807d1239364efcc8fe
Erik Bäcklund Ekvall <eriek286@li1-7.ida.liu.se>
2015-05-22 17:07:10 +0200

    globals for UI now exist

--- engine/globals.rkt
+++ engine/globals.rkt
@@ -2,6 +2,9 @@
 (provide (all-defined-out))
 
 (define *current-act* #f)
-
+(define *current-ui* #f)
 (define (transition destination) ;enkel implementering för tillfället
   (set! *current-act* destination)) ;så vi kan testa koden
+
+(define (change-ui new-ui) ;enkel implementering för tillfället
+  (set! *current-ui* new-ui)) ;så vi kan testa koden
