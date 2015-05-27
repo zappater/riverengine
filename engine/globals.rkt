16909b117537cfe27882ba2062df7dee33e7cc49
Henrik <henos134@student.liu.se>
2015-05-27 14:47:19 +0200

    started changing globals into a class

--- engine/globals.rkt
+++ engine/globals.rkt
@@ -4,6 +4,33 @@
 (define *control-scheme* #f)
 (define *current-act* #f)
 (define *current-ui* #f)
+(define *current-PC* #f)
+
+#| Nedan är menat att ersätta all annan kod i denna fil.
+På det här sättet går vi från några identiska globaler med identiska hjälpfunktioner till
+några få objekt som fungerar exakt lika.
+(define in-use%
+  (class object%
+    (init-field [object #f])
+    (define/public (change-me new-me)
+      (set! object new-me))
+    (define/public (get-obj)
+      object)))
+(define current-scheme
+  (new in-use%
+       [object #f]))
+(define current-act
+  (new in-use%
+       [object #f]))
+(define current-ui
+  (new in-use%
+       [object #f]))
+(define current-PC
+  (new in-use%
+       [object #f]))
+|#
+
+
 (define (transition destination) ;enkel implementering för tillfället
   (set! *current-act* destination)) ;så vi kan testa koden
 
@@ -12,3 +39,4 @@
 
 (define (change-control-scheme new-ctlse) ;enkel implementering för tillfället
   (set! *control-scheme* new-ctlse)) ;så vi kan testa koden
+
